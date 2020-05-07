d29f5aaf442493d01af08db22690b3b4a4ccc2f2
John Hammond <johnhammond010@gmail.com>
2020-05-06 20:26:24 -0400

    Made prepare.sh install requirements from loaded plugins

--- prepare.sh
+++ prepare.sh
@@ -2,3 +2,8 @@
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential python-dev python-pip libffi-dev
 pip install -r requirements.txt
+for d in CTFd/plugins/*; do \
+    if [ -f "$d/requirements.txt" ]; then \
+        pip install -r $d/requirements.txt --no-cache-dir; \
+    fi; \
+done;
